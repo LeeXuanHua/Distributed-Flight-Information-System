9a940dd3ace2e0a1e04f0715193cf7f3decaa72c
Lee Xuan Hua <lee_xuan_hua@yahoo.com.sg>
2023-03-09 18:12:23 +0800

    Instantiated FlightService as a service in bean and added code to test the database update

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
 import com.example.demo.models.*;
+import com.example.demo.services.FlightService;
 import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class Application {
@@ -20,6 +21,7 @@ public class Application {
     // Demonstrate the various functions and test the database
     @Bean
     CommandLineRunner commandLineRunner(
+            FlightService flightService,
             FlightInformationRepository flightInformationRepository,
             FlightBookingsRepository flightBookingsRepository,
             FlightMonitoringRepository flightMonitoringRepository) {
@@ -55,7 +57,7 @@ public class Application {
 
             flightInformationRepository.findAll().forEach(System.out::println);
 
-            flightInformationRepository.updateFlightsSeatAvailability(flightID, seatAvailability-5);
+            flightInformationRepository.updateFlightsSeatAvailability(flightID, seatAvailability - 5);
 
             flightInformationRepository.findFlightsBySrcAndDest(src, dest)
                     .forEach(System.out::println);
@@ -63,10 +65,10 @@ public class Application {
 
             // FlightBookingsRepository
             // Create a fake record
-            ClientID clientID = new ClientID((String)faker.internet().ipV4Address(), faker.number().numberBetween(1, 100));
+            ClientID clientID = new ClientID((String) faker.internet().ipV4Address(), faker.number().numberBetween(1, 100));
             int clientSeat = faker.number().numberBetween(1, 100);
 
-            ClientID clientID2 = new ClientID((String)faker.internet().ipV4Address(), faker.number().numberBetween(1, 100));
+            ClientID clientID2 = new ClientID((String) faker.internet().ipV4Address(), faker.number().numberBetween(1, 100));
             int clientSeat2 = faker.number().numberBetween(1, 100);
 
             FlightBookings flightBookings = new FlightBookings(
@@ -79,9 +81,9 @@ public class Application {
             flightBookingsRepository.findFlightBookingsByClientIDAndFlightID(clientID.getIP(), clientID.getPort(), flightID)
                     .ifPresent(s -> {
                         System.out.println(String.format("findFlightBookingsByClientIDAndFlightID %s: %s", clientID, s));
-            });
+                    });
 
-            flightBookingsRepository.incrementFlightBookings(clientID.getIP(), clientID.getPort(), flightID, clientSeat+10);
+            flightBookingsRepository.incrementFlightBookings(clientID.getIP(), clientID.getPort(), flightID, 10);
 
             flightBookingsRepository.insertFlightBookings(clientID2.getIP(), clientID2.getPort(), flightID, clientSeat2);
 
@@ -103,13 +105,32 @@ public class Application {
             flightMonitoringRepository.findFlightMonitoringByClientIDAndFlightID(clientID.getIP(), clientID.getPort(), flightID)
                     .ifPresent(s -> {
                         System.out.println(String.format("findFlightMonitoringByClientIDAndFlightID %s: %s", clientID, s));
-            });
+                    });
 
             flightMonitoringRepository.updateFlightMonitoringByClientIDAndFlightID(clientID.getIP(), clientID.getPort(), flightID, monitorInterval2);
 
             flightMonitoringRepository.insertFlightMonitoringByClientIDAndFlightID(clientID2.getIP(), clientID2.getPort(), flightID, monitorInterval2);
 
             flightMonitoringRepository.findAll().forEach(System.out::println);
+
+
+            // Add 1 flight via FlightService instead of FlightInformationRepository
+            int flightID_FS = faker.number().numberBetween(1, 100);
+            int clientSeat_FS = faker.number().numberBetween(1, 100);
+            ClientID clientID_FS = new ClientID((String) faker.internet().ipV4Address(), faker.number().numberBetween(1, 100));
+
+            System.out.println(String.format("Trying to add %s to flight %d with seat %d", clientID_FS, flightID_FS, clientSeat_FS));
+            flightService.AddFlightBooking(clientID_FS.getIP(), clientID_FS.getPort(), flightID_FS, clientSeat_FS);
+
+            // Service 1
+            System.out.println("Service 1");
+            List<FlightInformation> flights = flightService.GetFlightsBySourceAndDestination(src, dest);
+            flights.forEach(System.out::println);
+
+            // Service 2
+            System.out.println("Service 2");
+            Optional<FlightInformation> flight = flightService.GetFlightById(flightID_FS);
+            System.out.println(flight);
         };
     }
 }
